85d45eef02d61928eea5453390eb663922fcdfca
cmerrill01 <chasejm@byu.edu>
2023-10-05 17:38:14 -0600

    enhance(knight) implement moves

--- src/main/main/Knight.java
+++ src/main/main/Knight.java
@@ -26,12 +26,36 @@ public class Knight implements ChessPiece {
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        // create a set to hold possible moves
+        Collection<ChessPosition> possiblePositions = new HashSet<>();
+
+        // add all possible knight moves to the set
+        possiblePositions.add(new ChessPositionImpl(myPosition.getRow() - 2, myPosition.getColumn() - 1));
+        possiblePositions.add(new ChessPositionImpl(myPosition.getRow() - 2, myPosition.getColumn() + 1));
+        possiblePositions.add(new ChessPositionImpl(myPosition.getRow() - 1, myPosition.getColumn() - 2));
+        possiblePositions.add(new ChessPositionImpl(myPosition.getRow() - 1, myPosition.getColumn() + 2));
+        possiblePositions.add(new ChessPositionImpl(myPosition.getRow() + 1, myPosition.getColumn() - 2));
+        possiblePositions.add(new ChessPositionImpl(myPosition.getRow() + 1, myPosition.getColumn() + 2));
+        possiblePositions.add(new ChessPositionImpl(myPosition.getRow() + 2, myPosition.getColumn() - 1));
+        possiblePositions.add(new ChessPositionImpl(myPosition.getRow() + 2, myPosition.getColumn() + 1));
+
+        // create a separate set to store valid moves
         Collection<ChessMove> validMoves = new HashSet<>();
-        // set the current row and column equal to the start row and column
-        // decrement the current row by two
-        // decrement the current column by one
 
+        // check each move to ensure it is valid
+        for (ChessPosition toPosition : possiblePositions) {
+            // if the end position of a move is off the board, it is not valid
+            boolean valid = toPosition.onBoard(board);
+            if (valid) {
+                ChessMoveImpl move = new ChessMoveImpl(myPosition, toPosition);
+                // if the end position of a move is occupied by a non-enemy piece, it is not valid
+                if (board.getPiece(move.getEndPosition()) != null) {
+                    if (!move.isCapture(board)) valid = false;
+                }
+                if (valid) validMoves.add(move);
+            }
+        }
 
-        return null;
+        return validMoves;
     }
 }
