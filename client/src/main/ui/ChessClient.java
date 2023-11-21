eb0c1f474c9e7cae8173ef5e5a1a9428ba65b06b
cmerrill01 <chasejm@byu.edu>
2023-11-20 21:47:39 -0700

    enhance(ChessClient) implement register and login

--- client/src/main/ui/ChessClient.java
+++ client/src/main/ui/ChessClient.java
@@ -1,13 +1,18 @@
 package ui;
 
+import responses.LoginResponse;
+import responses.RegisterResponse;
+import server.ResponseException;
 import server.ServerFacade;
 
+import java.lang.module.ResolutionException;
 import java.util.Arrays;
 
 public class ChessClient {
 
     private final ServerFacade facade;
     private ClientState state = ClientState.LOGGEDOUT;
+    private String authToken;
 
     public ChessClient(String serverUrl) {
         facade = new ServerFacade(serverUrl);
@@ -17,25 +22,55 @@ public class ChessClient {
         var tokens = input.toLowerCase().split(" ");
         var command = (tokens.length > 0) ? tokens[0] : "help";
         var parameters = Arrays.copyOfRange(tokens, 1, tokens.length);
-        return switch (command) {
-            case "quit" -> "quit";
-            case "login" -> login(parameters);
-            case "register" -> register(parameters);
-            case "logout" -> logout();
-            case "create" -> createGame(parameters);
-            case "list" -> listGames();
-            case "join" -> joinGame(parameters);
-            case "observe" -> observeGame(parameters);
-            default -> help();
-        };
+        try {
+            return switch (command) {
+                case "quit" -> "quit";
+                case "login" -> login(parameters);
+                case "register" -> register(parameters);
+                case "logout" -> logout();
+                case "create" -> createGame(parameters);
+                case "list" -> listGames();
+                case "join" -> joinGame(parameters);
+                case "observe" -> observeGame(parameters);
+                default -> help();
+            };
+        } catch (ResponseException e) {
+            return e.getMessage();
+        }
+
     }
 
-    public String login(String... params) {
-        return null;
+    public String login(String... params) throws ResponseException {
+        if (params.length == 2) {
+            String username = params[0];
+            String password = params[1];
+            LoginResponse response = facade.login(username, password);
+            if (response.getAuthToken() != null && response.getUsername() != null) {
+                authToken = response.getAuthToken();
+                state = ClientState.LOGGEDIN;
+                return String.format("Successfully signed in as %s.", response.getUsername());
+            } else {
+                return String.format("Error: %s", response.getMessage());
+            }
+        }
+        throw new ResponseException(400, "Expected: <username> <password>");
     }
 
-    public String register(String... params) {
-        return null;
+    public String register(String... params) throws ResponseException {
+        if (params.length == 3) {
+            String username = params[0];
+            String password = params[1];
+            String email = params[2];
+            RegisterResponse response = facade.register(username, password, email);
+            if (response.getAuthToken() != null && response.getUsername() != null) {
+                authToken = response.getAuthToken();
+                state = ClientState.LOGGEDIN;
+                return String.format("Successfully registered new user: %s.", response.getUsername());
+            } else {
+                return String.format("Error: %s", response.getMessage());
+            }
+        }
+        throw new ResponseException(400, "Expected: <username> <password> <email>");
     }
 
     public String logout() {
